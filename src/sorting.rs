de07b237d8f3840ffd241023aafdaa7c3dccd931
thethongngu <nguyenthethong1996@gmail.com>
2022-10-05 12:58:39 +0800

    refactor test to prevent duplication

--- src/sorting.rs
+++ src/sorting.rs
@@ -1,4 +1,14 @@
-pub mod bubble_sort;
+mod bubble_sort;
+mod insertion_sort;
+mod selection_sort;
+
+pub trait Sorter<T> {
+    fn sort(&self, slice: &mut [T])
+    where
+        T: Ord;
+
+    fn name(&self) -> &'static str;
+}
 
 pub fn is_increasing<T>(arr: &[T]) -> bool
 where
@@ -19,15 +29,83 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::is_increasing;
+    use super::{
+        bubble_sort::BubbleSort, insertion_sort::InsertionSort, is_increasing,
+        selection_sort::SelectionSort, Sorter,
+    };
+
+    struct SortAlgo<T> {
+        algos: Vec<Box<dyn Sorter<T>>>,
+    }
+
+    impl<T> SortAlgo<T> {
+        fn new() -> Self {
+            let bubble = BubbleSort;
+            let insertion = InsertionSort;
+            let selection = SelectionSort;
+            SortAlgo {
+                algos: vec![Box::new(bubble), Box::new(insertion), Box::new(selection)],
+            }
+        }
+    }
 
     #[test]
-    fn all_cases() {
+    fn utility_function() {
         assert!(is_increasing(&[1, 2, 3])); // normal
         assert!(is_increasing(&[1, 2, 2])); // equal
         assert!(is_increasing::<i32>(&[])); // empty
         assert!(is_increasing(&[-1000])); // negative
         assert!(!is_increasing(&[1, 2, 1])); // not increaseing
         assert!(!is_increasing(&[-1, -2])); // negative
+    }
+
+    #[test]
+    fn normal() {
+        let sorts = SortAlgo::new();
+        for algo in sorts.algos {
+            let mut arr = vec![3, 7, 11, -4, 6, 1, 1];
+            algo.sort(&mut arr);
+            assert!(is_increasing(&arr), "{} error", algo.name());
+        }
+    }
+
+    #[test]
+    fn increasing() {
+        let sorts = SortAlgo::new();
+        for algo in sorts.algos {
+            let mut arr = vec![-3, 7, 9, 10, 20, 88];
+            algo.sort(&mut arr);
+            assert!(is_increasing(&arr), "{} error", algo.name());
+        }
+    }
+
+    #[test]
+    fn decreasing() {
+        let sorts = SortAlgo::new();
+        for algo in sorts.algos {
+            let mut arr = vec![88, 20, 10, 9, 7, -3];
+            algo.sort(&mut arr);
+            assert!(is_increasing(&arr), "{} error", algo.name());
+        }
+    }
+
+    #[test]
+    fn empty() {
+        let sorts = SortAlgo::new();
+        for algo in sorts.algos {
+            let mut arr = Vec::<i32>::new();
+            algo.sort(&mut arr);
+            assert!(is_increasing(&arr), "{} error", algo.name());
+        }
+    }
+
+    #[test]
+    fn one_element_only() {
+        let sorts = SortAlgo::new();
+        for algo in sorts.algos {
+            let mut arr = vec![4];
+            algo.sort(&mut arr);
+            assert!(is_increasing(&arr), "{} error", algo.name());
+        }
     }
 }
